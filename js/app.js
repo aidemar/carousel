4548db1b32371ec4ce3c405bb79d2757f5f34f4d
aidemar <aidemarmag@hotmail.com>
2018-01-18 18:08:38 -0600

    actualizando maquetado

--- js/app.js
+++ js/app.js
@@ -1,26 +1,49 @@
 $(document).ready(function() {
+    //elemento y evento para cambiar imagen a través de botones
   var $botones = $(".control")
 // console.log($botones);
   $botones.click(cargarImagen);
-  var target = 0;
-})
+ 
+//elementos y eventos para cambiar imagenes usando las flechas
+  var $previous = $(".previous");
+  var $next = $(".next");
+
+  $previous.click(previous);
+  $next.click(next);
+});
+
+var target = 0;
 
 var cargarImagen = function () {
-  var $target = $(this).data("target");
-  mostrarImagen($target);
-  changeColorButton($target);
-}
+  var target = $(this).data("target");
+  mostrarImagen(target);
+  changeColorButton(target);
+};
 
 var mostrarImagen = function(target) {
   var $actualSlide = $("div.active");
-  var $newImage = $("div[data-slide=" + target + "]");
+  var $newImage = $("div[data-slide='" + target + "']");
   $actualSlide.removeClass("active");
   $newImage.addClass("active");
-}
+};
 
-var changeColorButton = function(target) {
-  var $actualButton = $("button.active");
-  var $newButton = $("div[data-target=" + target + "]");
-  $actualButton.removeClass("active");
-  $button.addClass("active");
-}
+var changeColorButton = function (target) {
+    var $actualButton = $("button.active");
+    var $newButton = $("button[data-target='" + target +"']");
+    $actualButton.removeClass("active");
+    $newButton.addClass("active");
+};
+
+var previous = function () {
+  
+  var target = target - 1;
+  var target = (target < 0) ? 4 : target;
+  mostrarImagen(target);
+}; 
+
+var next = function (e) {
+  e.preventDefault();
+  var target = target + 1;
+  var target = (target > 4) ? 0 : target;
+  mostrarImagen(target);
+};
